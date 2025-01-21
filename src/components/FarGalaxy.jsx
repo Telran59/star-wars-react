74ff81bb3c28d314b6b50f646fa0afc47be0f364
edward <edwardlfish@gmail.com>
2025-01-21 17:26:49 +0200

    Add random opening crawl

--- src/components/FarGalaxy.jsx
+++ src/components/FarGalaxy.jsx
@@ -1,8 +1,21 @@
-import {openingCrawl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+import {base_url} from "../utils/constants.js";
+
 
 const FarGalaxy = () => {
+    const [openingCrawl, setOpeningCrawl] = useState('Loading...');
+
+    useEffect(() => {
+        const episode = Math.floor(1 + Math.random() * 6);
+        fetch(`${base_url}/v1/films/${episode}`)
+            .then(res => res.json())
+            .then(data => setOpeningCrawl(data.opening_crawl));
+    }, [])
+
     return (
-        <p className="farGalaxy">{openingCrawl}</p>
+        <p className="farGalaxy">
+            {openingCrawl}
+        </p>
     );
 };
 
