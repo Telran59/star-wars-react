abf7fad3d9d19064d6b71438cbd1407fd5aaee50
edward <edwardlfish@gmail.com>
2025-01-24 11:03:32 +0200

    Implements AboutMe and Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,52 @@
+import '../Contact.css'
+import {useEffect, useState} from "react";
+import {base_url, period_month} from "../utils/constants.js";
+
 const Contact = () => {
+    const [planets, setPlanets] = useState(['wait...']);
+
+    async function fillPlanets(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        const planets = data.map(item => item.name);
+        setPlanets(planets);
+        localStorage.setItem('planets', JSON.stringify({
+            payload: planets,
+            time: Date.now()
+        }));
+    }
+
+    useEffect(() => {
+        const planets = JSON.parse(localStorage.getItem('planets'));
+        if (planets && ((Date.now() - planets.time) < period_month)) {
+            setPlanets(planets.payload);
+        } else {
+            fillPlanets(`${base_url}/v1/planets`);
+        }
+    }, [])
+
     return (
-        <div>
-            Contact
-        </div>
-    );
+        <form className="container" onSubmit={e => {
+            e.preventDefault();
+        }}>
+            <label>First Name
+                <input type="text" name="firstname" placeholder="Your name.."/>
+            </label>
+            <label>Last Name
+                <input type="text" name="lastname" placeholder="Your last name.."/>
+            </label>
+            <label>Planet
+                <select name="planet">
+                    {planets.map(item => <option value={item} key={item}>{item}</option>)}
+                </select>
+            </label>
+
+            <label>Subject
+                <textarea name="subject" placeholder="Write something.."></textarea>
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    )
 };
 
 export default Contact;
